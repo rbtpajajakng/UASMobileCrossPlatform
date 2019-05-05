3909940afb0db5912133fd4c2504b13a3326efa3
rbtpajajakng <robert.pajajakng@gmail.com>
2019-05-05 17:18:47 +0700

    tambah firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import firebase from 'firebase'
 
 @Component({
   templateUrl: 'app.html'
@@ -12,6 +13,15 @@ export class MyApp {
   rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    var config = {
+      apiKey: "AIzaSyCIv3Q-IUTIIm67PtpJsBfMZbcK40C17CE",
+      authDomain: "uas-mobile-cp.firebaseapp.com",
+      databaseURL: "https://uas-mobile-cp.firebaseio.com",
+      projectId: "uas-mobile-cp",
+      storageBucket: "uas-mobile-cp.appspot.com",
+      messagingSenderId: "598975824582"
+    };
+    firebase.initializeApp(config);
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
