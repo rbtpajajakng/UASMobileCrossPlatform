7194ce0163b0156212b5245e99dd4fcacda05cae
natasyaw <42238267+natasyaw@users.noreply.github.com>
2019-05-20 18:38:03 +0700

    ubah desain login

---
+++ src/pages/login/login.ts
@@ -0,0 +1,65 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+import { User } from '../../models/user';
+import firebase from 'firebase';
+import{ AngularFireAuth} from 'angularfire2/auth';
+import { RegisterPage } from '../register/register';
+
+/**
+ * Generated class for the Home2Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  //user = {} as User;
+ 
+
+  constructor(private afAuth: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams, public alertCtrl :AlertController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  //Login(email,password){
+    //firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+      // Handle Errors here.
+      //var errorCode = error.code;
+      //var errorMessage = error.message;
+      // ...
+    //});
+    
+        //this.navCtrl.push(TabsPage);
+  
+    //}
+
+
+    async Login(email: string, password: string) {
+      try {
+        const result = await this.afAuth.auth.signInWithEmailAndPassword(email, password);
+        if (result) {
+          this.navCtrl.push(TabsPage);
+        }  
+      }
+      catch (e) {
+        console.error(e);
+      }
+      
+    }
+   
+
+    Register(){
+      this.navCtrl.push(RegisterPage);
+    }
+  }
+
+
